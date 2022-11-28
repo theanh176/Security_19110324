d194d56ebf02714b417e6867864d5373ca400887
theanh <tranluutheanh17601@gmail.com>
2022-11-28 14:40:00 +0700

    update

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-const https = require('https');
+const fs = require("fs");
+const https = require("https");
 const path = require("path");
 const express = require("express");
+const helmet = require("helmet");
 
 const PORT = 3000;
 
 const app = express();
+
+// app.use(helmet());
 
 app.get("/secret", (req, res) => {
 	return res.send("Your personal secret value is 42!");
@@ -14,9 +18,14 @@ app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-https.createServer({
-    // key
-    // cert: '',
-}).listen(PORT, () => {
-	console.log(`Server listening on port ${PORT}`);
-});
+https
+	.createServer(
+		{
+			key: fs.readFileSync("key.pem"),
+			cert: fs.readFileSync("cert.pem"),
+		},
+		app
+	)
+	.listen(PORT, () => {
+		console.log(`Server listening on port ${PORT}`);
+	});
