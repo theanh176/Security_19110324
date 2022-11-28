a0f7824c6854f01c7e3a135a6208b776061b2871
theanh <tranluutheanh17601@gmail.com>
2022-11-28 09:25:59 +0700

    create

---
+++ server.js
@@ -0,0 +1,22 @@
+const https = require('https');
+const path = require("path");
+const express = require("express");
+
+const PORT = 3000;
+
+const app = express();
+
+app.get("/secret", (req, res) => {
+	return res.send("Your personal secret value is 42!");
+});
+
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+https.createServer({
+    // key
+    // cert: '',
+}).listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`);
+});
